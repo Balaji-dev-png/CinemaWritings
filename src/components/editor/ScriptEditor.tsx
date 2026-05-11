d6f58f32847b1380ec72eef655aedd32d0c7b7b3
Balaji-dev-png <shaty768@gmail.com>
2026-05-11 11:50:01 +0530

    fix: remove redeclaration of canvasWidth

--- src/components/editor/ScriptEditor.tsx
+++ src/components/editor/ScriptEditor.tsx
@@ -335,9 +335,6 @@ export const ScriptEditor = ({
     const toolbarWidth = toolbarOrientation === "horizontal" ? 300 : 60;
     
     // Default: Pin to the right edge of the script page
-    const pageWidth = 816; // WGA Standard Width
-    const canvasWidth = window.innerWidth;
-    
     // The script is centered. Its right edge is:
     // center of screen + half of script width + 16px padding
     let x = (canvasWidth / 2) + (pageWidth / 2) + 16;
