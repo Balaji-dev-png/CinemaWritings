95d0f163bcb52f75fb4a755fd145480b3c3ec2e9
Balaji-dev-png <shaty768@gmail.com>
2026-08-28 18:38:41 +0530

    fix: explicit cookie_domain and remove head wrapper to fix hydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,12 +23,11 @@ export default function RootLayout({
       className="h-full antialiased"
       suppressHydrationWarning
     >
-      <head>
+      <body className="min-h-full flex flex-col font-sans transition-colors duration-500 bg-zinc-50 dark:bg-black" suppressHydrationWarning>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300;400;700&family=Courier+Prime:wght@400;700&family=Inter:wght@100..900&family=Lato:wght@300;400;700&family=Lora:wght@400..700&family=Montserrat:wght@100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Playfair+Display:wght@400..900&family=Poppins:wght@300;400;500;600;700&family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="min-h-full flex flex-col font-sans transition-colors duration-500 bg-zinc-50 dark:bg-black" suppressHydrationWarning>
+        
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-5Z7BE1JVD2"
@@ -39,7 +38,10 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-5Z7BE1JVD2');
+            gtag('config', 'G-5Z7BE1JVD2', {
+              cookie_domain: window.location.hostname,
+              cookie_flags: 'SameSite=Lax;Secure'
+            });
           `}
         </Script>
         <ThemeProvider attribute="data-theme" defaultTheme="system" enableSystem disableTransitionOnChange>
